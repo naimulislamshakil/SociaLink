e16c5eb9207c6984fd9d3a1615a1945eb3ffdac7
Naimul Islam <ms0413354@gmail.com>
2023-05-03 20:04:06 +0600

    add theme color and other

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import Footer from './pages/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Navbar from './pages/Navbar';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/feed" element={<Home />} />
+				<Route path="/profile/:id" element={<Profile />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
