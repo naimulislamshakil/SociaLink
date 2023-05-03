fcbb56738f0c8186e2fd355641d725f51cd5cac5
Naimul Islam <ms0413354@gmail.com>
2023-05-03 21:09:48 +0600

    fix some problem in navbar

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -1,4 +1,16 @@
-import { Box, IconButton, InputBase, useTheme } from '@mui/material';
+import {
+	Avatar,
+	Box,
+	Divider,
+	IconButton,
+	InputBase,
+	ListItemIcon,
+	Menu,
+	MenuItem,
+	Tooltip,
+	Typography,
+	useTheme,
+} from '@mui/material';
 import React, { useContext } from 'react';
 import { ColorModeContext, tokens } from '../../theme';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
@@ -7,11 +19,21 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import SearchIcon from '@mui/icons-material/Search';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
 
 const Navbar = () => {
 	const theme = useTheme();
 	const colorMode = useContext(ColorModeContext);
 	const colors = tokens(theme.palette.mode);
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
 	return (
 		<Box display="flex" justifyContent="space-between" p={2}>
@@ -42,9 +64,73 @@ const Navbar = () => {
 				<IconButton>
 					<SettingsOutlinedIcon />
 				</IconButton>
-				<IconButton>
-					<PersonOutlinedIcon />
-				</IconButton>
+				<Tooltip title="Account settings">
+					<IconButton
+						onClick={handleClick}
+						size="small"
+						sx={{ ml: 2 }}
+						aria-controls={open ? 'account-menu' : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? 'true' : undefined}
+					>
+						<PersonOutlinedIcon />
+					</IconButton>
+				</Tooltip>
+
+				<Menu
+					anchorEl={anchorEl}
+					id="account-menu"
+					open={open}
+					onClose={handleClose}
+					onClick={handleClose}
+					PaperProps={{
+						elevation: 0,
+						sx: {
+							overflow: 'visible',
+							filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+							mt: 1.5,
+							'& .MuiAvatar-root': {
+								width: 32,
+								height: 32,
+								ml: -0.5,
+								mr: 1,
+							},
+							'&:before': {
+								content: '""',
+								display: 'block',
+								position: 'absolute',
+								top: 0,
+								right: 14,
+								width: 10,
+								height: 10,
+								bgcolor: 'background.paper',
+								transform: 'translateY(-50%) rotate(45deg)',
+								zIndex: 0,
+							},
+						},
+					}}
+					transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+					anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+				>
+					<MenuItem onClick={handleClose}>
+						<Avatar /> Profile
+					</MenuItem>
+
+					<Divider />
+
+					<MenuItem onClick={handleClose}>
+						<ListItemIcon>
+							<Settings fontSize="small" />
+						</ListItemIcon>
+						Settings
+					</MenuItem>
+					<MenuItem onClick={handleClose}>
+						<ListItemIcon>
+							<Logout fontSize="small" />
+						</ListItemIcon>
+						Logout
+					</MenuItem>
+				</Menu>
 			</Box>
 		</Box>
 	);
