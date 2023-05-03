9731e87714933d5e687cf620c71127fab3fd1fa9
Naimul Islam <ms0413354@gmail.com>
2023-05-03 22:59:11 +0600

    login section complete

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,138 @@
+import {
+	Avatar,
+	Box,
+	Button,
+	Checkbox,
+	Container,
+	FormControlLabel,
+	Grid,
+	TextField,
+	Typography,
+	useTheme,
+} from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { tokens } from '../../theme';
+import { useFormik } from 'formik';
+import { Password } from '@mui/icons-material';
+import * as yup from 'yup';
 
 const Login = () => {
-	return <div>Login</div>;
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+
+	const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+		useFormik({
+			initialValues,
+			validationSchema: userSchema,
+			onSubmit: (values) => {
+				console.log(values);
+			},
+		});
+	return (
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+					<TextField
+						margin="normal"
+						required
+						color="secondary"
+						fullWidth
+						id="email"
+						value={values.email}
+						onBlur={handleBlur}
+						onChange={handleChange}
+						label="Email Address"
+						name="email"
+						error={!!touched.email && !!errors.email}
+						helperText={touched.email && errors.email}
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						color="secondary"
+						fullWidth
+						name="password"
+						value={values.password}
+						onBlur={handleBlur}
+						onChange={handleChange}
+						label="Password"
+						type="password"
+						id="password"
+						error={!!touched.password && !!errors.password}
+						helperText={touched.password && errors.password}
+						autoComplete="current-password"
+					/>
+					<FormControlLabel
+						control={
+							<Checkbox value="remember" style={{ color: colors.grey[100] }} />
+						}
+						label="Remember me"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+						style={{ color: colors.grey[100] }}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item xs>
+							<Link
+								href="#"
+								variant="body2"
+								style={{ color: colors.grey[100] }}
+							>
+								Forgot password?
+							</Link>
+						</Grid>
+						<Grid item>
+							<Link
+								to="/register"
+								variant="body2"
+								style={{ color: colors.grey[100] }}
+							>
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+		</Container>
+	);
 };
 
+const initialValues = {
+	email: '',
+	password: '',
+};
+
+const regularExpression =
+	/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+const userSchema = yup.object().shape({
+	email: yup.string().email('Invalid Email').required('Required'),
+	password: yup
+		.string()
+		.matches(regularExpression, 'Password Not Valid')
+		.required('Required'),
+});
 export default Login;
