eb1a0bc0e0683b443627ed265632140fe8828f3c
Naimul Islam <ms0413354@gmail.com>
2023-05-03 20:17:48 +0600

    enable dark and light mode

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import Navbar from './pages/Navbar';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 import { Route, Routes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
 
 function App() {
+	const [theme, colorMode] = useMode();
 	return (
-		<div className="app">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/feed" element={<Home />} />
-				<Route path="/profile/:id" element={<Profile />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-			<Footer />
-		</div>
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/feed" element={<Home />} />
+						<Route path="/profile/:id" element={<Profile />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+					<Footer />
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
 	);
 }
 
