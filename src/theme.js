0dd1c9ec1351daf82c566c21c9ef7e8491af4ea9
Naimul Islam <ms0413354@gmail.com>
2023-05-03 20:14:34 +0600

    navbar section complete

--- src/theme.js
+++ src/theme.js
@@ -1,38 +1,128 @@
 import { createTheme } from '@mui/material/styles';
 import { createContext, useMemo, useState } from 'react';
 
-export const colorTokens = {
-	grey: {
-		0: '#FFFFFF',
-		10: '#F6F6F6',
-		50: '#F0F0F0',
-		100: '#E0E0E0',
-		200: '#C2C2C2',
-		300: '#A3A3A3',
-		400: '#858585',
-		500: '#666666',
-		600: '#4D4D4D',
-		700: '#333333',
-		800: '#1A1A1A',
-		900: '#0A0A0A',
-		1000: '#000000',
-	},
-	primary: {
-		50: '#E6FBFF',
-		100: '#CCF7FE',
-		200: '#99EEFD',
-		300: '#66E6FC',
-		400: '#33DDFB',
-		500: '#00D5FA',
-		600: '#00A0BC',
-		700: '#006B7D',
-		800: '#00353F',
-		900: '#001519',
-	},
-};
+// color design tokens export
+export const tokens = (mode) => ({
+	...(mode === 'dark'
+		? {
+				grey: {
+					100: '#e0e0e0',
+					200: '#c2c2c2',
+					300: '#a3a3a3',
+					400: '#858585',
+					500: '#666666',
+					600: '#525252',
+					700: '#3d3d3d',
+					800: '#292929',
+					900: '#141414',
+				},
+				primary: {
+					100: '#d0d1d5',
+					200: '#a1a4ab',
+					300: '#727681',
+					400: '#1F2A40',
+					500: '#141b2d',
+					600: '#101624',
+					700: '#0c101b',
+					800: '#080b12',
+					900: '#040509',
+				},
+				greenAccent: {
+					100: '#dbf5ee',
+					200: '#b7ebde',
+					300: '#94e2cd',
+					400: '#70d8bd',
+					500: '#4cceac',
+					600: '#3da58a',
+					700: '#2e7c67',
+					800: '#1e5245',
+					900: '#0f2922',
+				},
+				redAccent: {
+					100: '#f8dcdb',
+					200: '#f1b9b7',
+					300: '#e99592',
+					400: '#e2726e',
+					500: '#db4f4a',
+					600: '#af3f3b',
+					700: '#832f2c',
+					800: '#58201e',
+					900: '#2c100f',
+				},
+				blueAccent: {
+					100: '#e1e2fe',
+					200: '#c3c6fd',
+					300: '#a4a9fc',
+					400: '#868dfb',
+					500: '#6870fa',
+					600: '#535ac8',
+					700: '#3e4396',
+					800: '#2a2d64',
+					900: '#151632',
+				},
+		  }
+		: {
+				grey: {
+					100: '#141414',
+					200: '#292929',
+					300: '#3d3d3d',
+					400: '#525252',
+					500: '#666666',
+					600: '#858585',
+					700: '#a3a3a3',
+					800: '#c2c2c2',
+					900: '#e0e0e0',
+				},
+				primary: {
+					100: '#040509',
+					200: '#080b12',
+					300: '#0c101b',
+					400: '#f2f0f0', // manually changed
+					500: '#141b2d',
+					600: '#1F2A40',
+					700: '#727681',
+					800: '#a1a4ab',
+					900: '#d0d1d5',
+				},
+				greenAccent: {
+					100: '#0f2922',
+					200: '#1e5245',
+					300: '#2e7c67',
+					400: '#3da58a',
+					500: '#4cceac',
+					600: '#70d8bd',
+					700: '#94e2cd',
+					800: '#b7ebde',
+					900: '#dbf5ee',
+				},
+				redAccent: {
+					100: '#2c100f',
+					200: '#58201e',
+					300: '#832f2c',
+					400: '#af3f3b',
+					500: '#db4f4a',
+					600: '#e2726e',
+					700: '#e99592',
+					800: '#f1b9b7',
+					900: '#f8dcdb',
+				},
+				blueAccent: {
+					100: '#151632',
+					200: '#2a2d64',
+					300: '#3e4396',
+					400: '#535ac8',
+					500: '#6870fa',
+					600: '#868dfb',
+					700: '#a4a9fc',
+					800: '#c3c6fd',
+					900: '#e1e2fe',
+				},
+		  }),
+});
 
 // mui theme settings
 export const themeSettings = (mode) => {
+	const colors = tokens(mode);
 	return {
 		palette: {
 			mode: mode,
@@ -40,39 +130,35 @@ export const themeSettings = (mode) => {
 				? {
 						// palette values for dark mode
 						primary: {
-							dark: colorTokens.primary[200],
-							main: colorTokens.primary[500],
-							light: colorTokens.primary[800],
+							main: colors.primary[500],
+						},
+						secondary: {
+							main: colors.greenAccent[500],
 						},
 						neutral: {
-							dark: colorTokens.grey[100],
-							main: colorTokens.grey[200],
-							mediumMain: colorTokens.grey[300],
-							medium: colorTokens.grey[400],
-							light: colorTokens.grey[700],
+							dark: colors.grey[700],
+							main: colors.grey[500],
+							light: colors.grey[100],
 						},
 						background: {
-							default: colorTokens.grey[900],
-							alt: colorTokens.grey[800],
+							default: colors.primary[500],
 						},
 				  }
 				: {
 						// palette values for light mode
 						primary: {
-							dark: colorTokens.primary[700],
-							main: colorTokens.primary[500],
-							light: colorTokens.primary[50],
+							main: colors.primary[100],
+						},
+						secondary: {
+							main: colors.greenAccent[500],
 						},
 						neutral: {
-							dark: colorTokens.grey[700],
-							main: colorTokens.grey[500],
-							mediumMain: colorTokens.grey[400],
-							medium: colorTokens.grey[300],
-							light: colorTokens.grey[50],
+							dark: colors.grey[700],
+							main: colors.grey[500],
+							light: colors.grey[100],
 						},
 						background: {
-							default: colorTokens.grey[10],
-							alt: colorTokens.grey[0],
+							default: '#fcfcfc',
 						},
 				  }),
 		},
