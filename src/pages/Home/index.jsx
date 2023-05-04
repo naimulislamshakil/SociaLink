9e9fe864d6357c900f0eac2335f60838841b81f2
Naimul Islam <ms0413354@gmail.com>
2023-05-04 16:29:51 +0600

    Home section using grid

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,32 @@
+import { Box, Grid, Typography, useMediaQuery } from '@mui/material';
+import Profile from '../../components/Profile';
 import React from 'react';
+import Post from '../../components/Post';
+import FriendList from '../../components/FriendList';
 
 const Home = () => {
-	return <div>Home</div>;
+	const isMobile = useMediaQuery('(min-width:600px)');
+	return (
+		<Box>
+			<Box
+				width="100%"
+				padding="2rem 6%"
+				display={isMobile ? 'flex' : 'block'}
+				gap="0.5rem"
+				justifyContent="space-between"
+			>
+				<Grid md={4}>
+					<Profile />
+				</Grid>
+				<Grid md={4}>
+					<Post />
+				</Grid>
+				<Grid md={4}>
+					<FriendList />
+				</Grid>
+			</Box>
+		</Box>
+	);
 };
 
 export default Home;
